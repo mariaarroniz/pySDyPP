4e73579f59d407bef5353b8484a5770e2eb8a0f2
mariaarroniz <maria.arroniz@alumnos.uneatlantico.es>
2025-12-07 12:10:15 +0100

    docs: added context and updated controller (EFCore)

---
+++ documentos/03-visual/controlDiabeticos/controlDiabeticos/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using controlDiabeticos.Models;
+
+namespace controlDiabeticos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Paciente> pacientes { get; set; }
+    }
+}
--- documentos/03-visual/controlDiabeticos/controlDiabeticos/Program.cs
+++ documentos/03-visual/controlDiabeticos/controlDiabeticos/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using controlDiabeticos.Data;
 
 namespace controlDiabeticos
 {
@@ -8,8 +10,10 @@ namespace controlDiabeticos
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
